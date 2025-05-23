2dbfe66f80ef5423c5f969a161ce29aab7f6c767
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-05-23 09:10:32 -0500

    Package Added

--- MiniAPI.Tests/obj/Debug/net9.0/MiniAPI.Tests.AssemblyInfo.cs
+++ MiniAPI.Tests/obj/Debug/net9.0/MiniAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe14a42bb90bf6e75407562f490dd61ecfd2358f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92a1438daf52168d8beaa8d5e231ed2a68707f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
