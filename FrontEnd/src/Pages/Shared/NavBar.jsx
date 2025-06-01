ce3e8e2c4521610ad67c477d4260edefd7f8899e
hillbillyjoe41 <157071173+hillbillyjoe41@users.noreply.github.com>
2025-06-01 14:54:41 -0500

    Added Navigation Bar

---
+++ FrontEnd/src/Pages/Shared/NavBar.jsx
@@ -0,0 +1,44 @@
+import { link } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext";
+import { Popup } from "reactjs-popup";
+import { AppBar, Container, Toolbar, Box, Button, IconButton, Typography } from "@mui/material";
+
+const NavBar = () => {
+    const { user, logout } = useAuth();
+
+    return (
+        <AppBar>
+            <Container>
+                <Toolbar>
+
+                </Toolbar>
+                <Box>
+
+                </Box>
+                <Box>
+                    {user?.id ? (
+                    <Button >Logout</Button>
+                    ) : (<Popup
+                        trigger={<Button sx={{ color: 'white' }}>Login</Button>}
+                        modal
+                        nested
+                        overlayStyle={{ background: "rgba(0, 0, 0, 0.5)" }}
+                        contentStyle={{
+                            backgroundColor: "#f8f9fa",
+                            borderRadius: "10px",
+                            padding: "30px",
+                            maxWidth: "450px",
+                            margin: "100px auto",
+                            boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.2)",
+                            fontFamily: "Arial, sans-serif",
+                        }}
+                    >
+                        Login
+                    </Popup>)}
+                </Box>
+            </Container>
+        </AppBar>
+    )
+}
+
+export default NavBar
